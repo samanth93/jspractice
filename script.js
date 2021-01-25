2d9c93510818c7bb173f3c0ebbc97390bed7e153
samanth93 <samanth.sid@gmail.com>
2021-01-25 15:09:49 -0500

    function returning function

--- script.js
+++ script.js
@@ -5,9 +5,20 @@ const firstLetterUpper = function (str) {
   return [first.toUpperCase(), ...remaining].join(" ");
 };
 
+// function accepting callback
 const transform = function (str, fn) {
   console.log(`Given string ${str}`);
   console.log(`Transformed string ${fn(str)}`);
 };
 
 transform("hello", firstLetterUpper);
+
+// function returning function
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const x = greet("hey");
+x("samanth");
