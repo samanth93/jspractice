965ca6a92a6fee57852da3c2eb8232253defeea3
samanth93 <samanth.sid@gmail.com>
2021-01-30 15:59:08 -0500

    Closures

--- script.js
+++ script.js
@@ -146,35 +146,57 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 GOOD LUCK 😀
 */
 
-const poll = {
-  question: "what is your favourite programming language ?",
-  options: ["0: Javascript", "1: Python", "2: Rust", "3: C++"],
-  answers: new Array(4).fill(0),
-  registerNewAnswer: function () {
-    const answer = Number(
-      prompt(
-        `${this.question}\n${this.options.join("\n")}\n(Write option number)`
-      )
-    );
-    // shortcircuiting
-    typeof answer === "number" &&
-      answer < this.answers.length &&
-      this.answers[answer]++;
-    // displayResults();
-    // console.log(this.answers)
-    this.displayResults();
-    this.displayResults("string");
-  },
-  displayResults: function (type = "array") {
-    if (type == "array") {
-      console.log(this.answers);
-    } else if (type == "string") {
-      console.log(`Ploll results are ${this.answers.join(",")}`);
-    }
-  },
-};
-document
-  .querySelector(".poll")
-  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+// const poll = {
+//   question: "what is your favourite programming language ?",
+//   options: ["0: Javascript", "1: Python", "2: Rust", "3: C++"],
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer: function () {
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+//       )
+//     );
+//     // shortcircuiting
+//     typeof answer === "number" &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
+//     // displayResults();
+//     // console.log(this.answers)
+//     this.displayResults();
+//     this.displayResults("string");
+//   },
+//   displayResults: function (type = "array") {
+//     if (type == "array") {
+//       console.log(this.answers);
+//     } else if (type == "string") {
+//       console.log(`Ploll results are ${this.answers.join(",")}`);
+//     }
+//   },
+// };
+// document
+//   .querySelector(".poll")
+//   .addEventListener("click", poll.registerNewAnswer.bind(poll));
 
-// const displayResults = function (type) {};
+// // call method is used to manually set the this method
+// poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') 
+to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly 
+the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+  document.querySelector("body").addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
