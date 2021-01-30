3099b2703230f98410403047be8a05ea46fdcb2e
samanth93 <samanth.sid@gmail.com>
2021-01-30 14:32:10 -0500

    Poll Challenge

--- script.js
+++ script.js
@@ -119,7 +119,8 @@
 
 /* 
 Let's build a simple poll app!
-A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data 
+is stored in the starter object below.
 Here are your tasks:
 1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
   1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
@@ -130,12 +131,16 @@ Here are your tasks:
         3: C++
         (Write option number)
   
-  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 
+  1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
 2. Call this method whenever the user clicks the "Answer poll" button.
-3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be 
+either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default 
+option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
 HINT: Use many of the tools you learned about in this and the last section 😉
-BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put 
+the arrays in the poll object! So what shoud the this keyword look like in this situation?
 BONUS TEST DATA 1: [5, 2, 3]
 BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 GOOD LUCK 😀
@@ -145,24 +150,31 @@ const poll = {
   question: "what is your favourite programming language ?",
   options: ["0: Javascript", "1: Python", "2: Rust", "3: C++"],
   answers: new Array(4).fill(0),
-};
-
-poll.registerNewAnswer = function () {
-  console.log(this);
-  this.answers[
-    parseInt(
-      prompt(`What is your favourite programming language?
-    0: JavaScript
-    1: Python
-    2: Rust
-    3: C++
-    (Write option number)`)
-    )
-  ]++;
-  displayResults();
+  registerNewAnswer: function () {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+      )
+    );
+    // shortcircuiting
+    typeof answer === "number" &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    // displayResults();
+    // console.log(this.answers)
+    this.displayResults();
+    this.displayResults("string");
+  },
+  displayResults: function (type = "array") {
+    if (type == "array") {
+      console.log(this.answers);
+    } else if (type == "string") {
+      console.log(`Ploll results are ${this.answers.join(",")}`);
+    }
+  },
 };
 document
   .querySelector(".poll")
   .addEventListener("click", poll.registerNewAnswer.bind(poll));
 
-const displayResults = function () {};
+// const displayResults = function (type) {};
